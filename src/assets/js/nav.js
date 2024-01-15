98f6c4079ce7d29089a92a4af6eff7a2795e4882
SovietBeatle <kolby.ellis123@gmail.com>
2024-01-15 02:06:19 -0600

    Transferred more content from old site

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -34,3 +34,12 @@ document.addEventListener('DOMContentLoaded', function() {
 		}
 	});
 	
+    const faqItems = Array.from(document.querySelectorAll('.cs-faq-item'));
+    for (const item of faqItems) {
+        const onClick = () => {
+        item.classList.toggle('active')
+    }
+    item.addEventListener('click', onClick)
+    }
+
+                                
